8d1286cdf03827a2db8a3f59aca2808f7c64a370
Austin Guevara <austin@guevaras.net>
2023-06-27 09:22:52 -0400

    Initial Commit

---
+++ Lunar Pilot 2.0/Lunar_Pilot_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Lunar_Pilot_2_0App.swift
+//  Lunar Pilot 2.0
+//
+//  Created by Austin Guevara on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Lunar_Pilot_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lunar Pilot 2.0UITests/Lunar_Pilot_2_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lunar_Pilot_2_0UITestsLaunchTests.swift
+//  Lunar Pilot 2.0UITests
+//
+//  Created by Austin Guevara on 6/27/23.
+//
+
+import XCTest
+
+final class Lunar_Pilot_2_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
